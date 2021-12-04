ed7aa4650eaaeefdbd22e341ae7115eeda8ff36a
jakesurry <jacobsurrywindows500@gmail.com>
2021-12-04 10:49:43 -0500

    Optimized graph operation, added zoom

--- calc.py
+++ calc.py
@@ -1,83 +1,129 @@
-from numpy import sign
+from numpy import sign, arange, meshgrid, vectorize, stack, where, isfinite
+import numpy as np
 import func_input
 import pygame as pg
-pg.init()
+import time
 
-SCREEN_SIZE = 800
+class Calculator():
+    def __init__(self):
 
-display = pg.display.set_mode((800,800))
-pg.display.set_caption('Calc')
+        pg.init()
+        pg.font.init()
 
-axis_surface = pg.Surface((800, 800))
+        self.change_scale(func_input.scale())
 
-func_surface = pg.Surface((800, 800))
-func_surface.set_colorkey((0, 0, 0))
+        self.recursions = func_input.recursions()
 
-scale = func_input.scale()
-step = 800/scale
+        self.font = pg.font.Font('Cambria.ttf', 20)
 
-def init_axis():
-    axis_surface.fill((255, 255, 255))
-    for x in range(40):
-        if x%5 == 0:
-            pg.draw.rect(axis_surface, (150, 150, 150), (x*20, 0, 1, 800))
-        else:
-            pg.draw.rect(axis_surface, (220, 220, 220), (x*20, 0, 1, 800))
+        self.display = pg.display.set_mode((800,800))
+        pg.display.set_caption('Calc')
 
-    for y in range(40):
-        if y%5 == 0:
-            pg.draw.rect(axis_surface, (150, 150, 150), (0, y*20, 800, 1))
-        else:
-            pg.draw.rect(axis_surface, (220, 220, 220), (0, y*20, 800, 1))
+        self.axis_surface = self.init_axis()
 
-    pg.draw.rect(axis_surface, (0, 0, 0), (400, 0, 1, 800))
-    pg.draw.rect(axis_surface, (0, 0, 0), (0, 400, 800, 1))
+        self.func_surface = pg.Surface((800, 800))
+        self.func_surface.set_colorkey((0, 0, 0))
 
-    return axis_surface
+    def change_scale(self, scale):
+        self.scale = scale
+        self.step = 800/self.scale
 
-def equal(x, y, func):
-        if type(func(x, y)) == bool:
-            return func(x, y)
+    def init_axis(self):
+        axis_surface = pg.Surface((800, 800))
 
-        else:
-            amnt = sign(func(x-1/step, y-1/step)) + sign(func(x, y)) + sign(func(x-1/step, y)) + sign(func(x, y-1/step))
-            if amnt < 4 and amnt > -4:
-                return True
+        axis_surface.fill((255, 255, 255))
+        for x in range(40):
+            if x%5 == 0:
+                pg.draw.rect(axis_surface, (150, 150, 150), (x*20, 0, 1, 800))
+            else:
+                pg.draw.rect(axis_surface, (220, 220, 220), (x*20, 0, 1, 800))
 
-def graph(functions, func):
-    #Need this for window to show, guess it's a pygame thing
-    for event in pg.event.get():
-        pass
+        for y in range(40):
+            if y%5 == 0:
+                pg.draw.rect(axis_surface, (150, 150, 150), (0, y*20, 800, 1))
+            else:
+                pg.draw.rect(axis_surface, (220, 220, 220), (0, y*20, 800, 1))
 
-    for x in range(800):
-        for y in range(800):
-            try:
-                if equal((x/step)-(scale/2), (scale/2)-(y/step), func):
-                    pg.draw.rect(func_surface, functions[func], (x, y, 1, 1))
-            except (ValueError, ZeroDivisionError, TypeError) as e:
-                pass
+        '''
+        for num in range(40):
+            if num%5 == 0:
+                print(self.scale, num)
+                self.axis_num((axis_surface, self.scale*(num/40)) - (self.scale/2), num*20+5, 400)
+                self.axis_num((axis_surface, self.scale*(num/40)) - (self.scale/2), 405, num*20)
+        '''
 
-        display.blit(axis_surface, (0, 0))
-        pg.draw.rect(display, (255, 0, 0), (x, 0, 1, 800))
-        display.blit(func_surface, (0, 0))
+        pg.draw.rect(axis_surface, (0, 0, 0), (400, 0, 1, 800))
+        pg.draw.rect(axis_surface, (0, 0, 0), (0, 400, 800, 1))
+
+        return axis_surface
+
+    '''
+    def axis_num(self, axis_surface, num, x, y):
+        num_surface = self.font.render(str(num), True, (0, 0, 0))
+        axis_surface.blit(num_surface, (x, y))
+    '''
+
+    def clear(self):
+        self.func_surface.fill((0, 0, 0))
+        self.display.blit(self.axis_surface, (0, 0))
         pg.display.update()
 
+    def graph(self, functions, func):
+            #Need this for window to show, guess it's a pygame thing
+            for k in range(self.recursions):
+
+                for event in pg.event.get():
+                    pass
+
+                self.display.blit(self.axis_surface, (0, 0)) 
+                pg.display.update()
+
+                xs, ys = meshgrid(arange(-.5, 801), arange(-.5, 801))
+                signs0 = sign(func(xs/self.step-self.scale/2, ys/self.step-self.scale/2, k))
+                signs1 = signs0[:-1, :-1]
+                signs2 = signs0[1:, :-1]
+                signs3 = signs0[:-1, 1:]
+
+                grid = where(isfinite(signs1) & ((signs1 != signs2) | (signs1 != signs3)), 1, 0)
+
+                ys, xs = np.where(grid != 0)
+
+                for (x, y) in zip(xs, ys):
+                    pg.draw.rect(self.func_surface, functions[func], (x, 800-y, 1, 1))
+
+                self.display.blit(self.func_surface, (0, 0))
+                pg.display.update()
+
+            self.recursions = func_input.recursions()
 def main():
-    axis_surface = init_axis()
+    calculator = Calculator()
 
     functions = func_input.get_functions()
 
     for func in functions:
-        graph(functions, func)    
-
-    pg.display.update()
+        calculator.graph(functions, func)    
 
     clock = pg.time.Clock()
     running = True
     while running:
+        
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    calculator.change_scale(calculator.scale * .8)
+                    for func in functions:
+                        calculator.clear()
+                        calculator.graph(functions, func)
+
+                if event.key == pg.K_DOWN:
+                    calculator.change_scale(calculator.scale * 1.2)
+                    for func in functions:
+                        calculator.clear()
+                        calculator.graph(functions, func)
+
         clock.tick(60)
 
 if __name__ == '__main__':
--- func_input.py
+++ func_input.py
@@ -1,5 +1,4 @@
-from math import *
-
+from numpy import *
 
 '''
 Write functions in the format shown below, then add them to the "functions" dictionary for them to be graphed.
@@ -11,64 +10,67 @@ The value of the functions in the dictionary is the RGB value of the color it wi
 #--------------------------------------------------------------------------------------------------------------------------------#
 
 SCALE = 20
+RECURSIONS = 2
 
-def linear(x, y):
+def linear(x, y, k):
 
     return x - y
 
-def sqr_root(x, y):
+def sqr_root(x, y, k):
 
     return sqrt(x)-y
 
-def radical(x, y):
+def radical(x, y, k):
 
     return x*y - 1
 
-def quadratic(x, y):
+def quadratic(x, y, k):
 
     return x**2-y
 
-def poly(x, y):
+def poly(x, y, k):
 
     return x**3+x**2+x-y
 
-def circle(x, y):
+def circle(x, y, k):
 
     return x*x + y*y - 50
 
-def full_circle(x, y):
+def full_circle(x, y, k):
 
     return y*y + x*x <= 50
 
-def squircles(x, y):
+def squircles(x, y, k):
 
     return sin(x)+cos(y) - .1
 
-def spam(x, y):
+def spam(x, y, k):
 
     return sin(cos(tan(x)) + sin(cos(tan(y)))) - sin(cos(tan(x*y)))
 
-def golden_ratio(x, y):
+def golden_ratio(x, y, k):
 
     r = sqrt(x*x + y*y)
-    theta = atan2(y, x)
+    theta = arctan2(y, x)
 
-    i = 0
+    return 1*exp(log(1.618)/(3.14/2)*(theta+6.28*k))-r
 
-    return 1*exp(log(1.618)/(3.14/2)*(theta+6.28*i))-r
+def test(x, y, k):
+    return y
 
 functions = {
 
-circle: (0, 255, 0),
+full_circle: (255, 0, 0),
 
 }
 
 #--------------------------------------------------------------------------------------------------------------------------------#
 
-
 #This just gives the scale to the graphing calculator, you should not need to edit this.
 def scale():
     return SCALE
+def recursions():
+    return RECURSIONS
 #This just gives the dictionary of functions to the graphing calculator, you should not need to edit this.
 def get_functions():
     return functions
